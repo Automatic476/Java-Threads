2e82d01a7de0c29f4cc87ee3232e27739be5d35a
“Jason <jasongz11@gmail.com>
2020-10-01 10:56:42 -0400

    End of Step 3
    Struck on step 3, stuck on our printout of arguments

--- Java-Threads/src/SimpleThread.java
+++ Java-Threads/src/SimpleThread.java
@@ -7,32 +7,27 @@
  * @author andrianoff
  * @version 15 Feb 2018
  */
-public class SimpleThread extends Thread
-{
+public class SimpleThread extends Thread {
 	/**
 	 * Constructs a SimpleThread with name name
+	 * 
 	 * @param name name of thread
 	 */
-	public SimpleThread(String name)
-	{
+	public SimpleThread(String name) {
 		super(name);
 	}
-	
+
 	/**
 	 * Thread's run method.<br>
 	 * Displays name 5 times then declares "Done!"
 	 */
-	public void run()
-	{
-		for (int i = 0; i < 5; i++)
-		{
+	public void run() {
+		for (int i = 0; i < 5; i++) {
 			System.out.println(i + " " + getName());
-			try
-			{
+			try {
 				sleep((int) (Math.random() * 1000));
-			} 
-			catch (InterruptedException e)
-			{}
+			} catch (InterruptedException e) {
+			}
 		}
 		System.out.println("Done! " + getName());
 	}
@@ -40,12 +35,22 @@ public class SimpleThread extends Thread
 	/**
 	 * The main method creates two threads and launches them.
 	 */
-	public static void main(String[] args)
-	{
-		SimpleThread st1 = new SimpleThread("Windows");
-		SimpleThread st2 = new SimpleThread("Linux");
+	public static void main(String[] args) {
 
-		st1.start();
-		st2.start();
+	//	String file = args[args.length - 1];
+		SimpleThread[] values = new SimpleThread[args.length];
+		//System.out.println(file);
+		
+		for (int i = 0; i < values.length; i++) {
+
+			System.out.println(values[i]);
+			//values[i].start(); 
+		}
+
+		// SimpleThread st1 = new SimpleThread("Windows");
+		// SimpleThread st2 = new SimpleThread("Linux");
+
+		// st1.start();
+		// st2.start();
 	}
-} 
+}
